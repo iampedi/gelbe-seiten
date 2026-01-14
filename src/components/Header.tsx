b99a24d995b8dcabcd6e725869f358ba1abae871
Pedram Ghane <p.ghane@gmail.com>
2026-01-14 13:08:05 +0100

    feat: Hero Section done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-import { Container } from "@/components/Container";
-import { Button } from "@/components/ui/button";
-import { PersonStandingIcon } from "lucide-react";
+import { Container } from '@/components/Container';
+import { Button } from '@/components/ui/button';
+import { PersonStandingIcon } from 'lucide-react';
 
 interface HeaderProps {
   className?: string;
@@ -10,14 +10,14 @@ interface HeaderProps {
 
 export const Header = ({ className }: HeaderProps) => {
   return (
-    <header className={cn("bg-secondary py-10 transition-colors", className)}>
+    <header className={cn('bg-secondary py-8 transition-colors', className)}>
       <Container>
-        <div className="flex justify-between items-center gap-8">
+        <div className="flex items-center justify-between gap-6">
           <div className="logo flex-1">
             <h1 className="text-4xl font-bold text-white">Gelbe Seiten</h1>
           </div>
 
-          <nav className="flex gap-6 font-medium text-xl text-white">
+          <nav className="flex gap-8 text-lg font-medium text-white">
             <a href="#" className="hover:underline">
               Suchen
             </a>
@@ -29,10 +29,15 @@ export const Header = ({ className }: HeaderProps) => {
             </a>
           </nav>
 
-          <div className="tools flex gap-2 items-center">
+          <div className="tools flex items-center gap-4">
             <Button>Firma eintragen</Button>
-            <Button size="icon">
-              <PersonStandingIcon className="size-7!" />
+
+            <Button
+              size="icon"
+              variant="outline"
+              className="size-8 rounded-full border-white"
+            >
+              <PersonStandingIcon className="size-5!" />
             </Button>
           </div>
         </div>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,21 +1,91 @@
-import { Header } from "./Header";
+import { MouseIcon, SearchIcon } from 'lucide-react';
+import { Container } from './Container';
+import { Header } from './Header';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
 
 export const HeroSection = () => {
   return (
     <div className="relative min-h-screen">
-      <Header className="absolute top-0 z-10 w-full bg-transparent" />
-
       <section className="absolute inset-0 size-full bg-black">
         <img
           src="/images/hero-bg.jpg"
           alt="a woman is working in a cafe"
           className="size-full object-cover"
         />
-        <div className="absolute inset-0  bg-linear-to-t from-black to-secondary/30" />
+        <div className="to-secondary/30 absolute inset-0 bg-linear-to-t from-black" />
       </section>
 
-      <div className="relative z-0 pt-32 container mx-auto">
-        {/* Hero text/content would go here */}
+      <div className="absolute z-10 flex size-full flex-col">
+        <Header className="bg-transparent" />
+
+        <div className="relative flex flex-1 items-center justify-center">
+          <Container>
+            <div className="mx-auto flex w-2/3 flex-col gap-4">
+              <h2 className="text-primary-foreground/90 text-center text-2xl font-normal uppercase">
+                Dienstleister schneller finden
+              </h2>
+
+              <div className="group flex rounded-full bg-white/50 px-4 py-2">
+                <Input
+                  placeholder="Was"
+                  className="rounded-none border-0 text-center text-lg text-white shadow-none placeholder:text-lg placeholder:text-white focus:placeholder-transparent focus-visible:ring-transparent"
+                />
+
+                <Input
+                  placeholder="Wo"
+                  className="rounded-none border-0 text-center text-lg text-white shadow-none placeholder:text-lg placeholder:text-white focus:placeholder-transparent focus-visible:ring-transparent"
+                />
+
+                <Button
+                  variant="ghost"
+                  className="min-w-24 justify-end gap-1 p-0 hover:bg-transparent focus-visible:ring-transparent"
+                >
+                  <SearchIcon className="group-hover:text-secondary size-7! text-white transition-all duration-500 ease-out" />
+                  <span className="max-w-0 translate-x-[-5px] overflow-hidden whitespace-nowrap opacity-0 transition-all duration-500 ease-out group-hover:max-w-[80px] group-hover:translate-x-0 group-hover:opacity-100">
+                    Finden
+                  </span>
+                </Button>
+              </div>
+
+              <div className="mt-10 flex items-center justify-center gap-6">
+                <div>
+                  <h2 className="text-primary-foreground/90 text-center font-light">
+                    Mehrfach <span className="font-normal">ausgezeichnet</span>
+                  </h2>
+                </div>
+
+                <div className="flex h-10 items-center justify-center gap-4">
+                  <div className="flex h-full items-center">
+                    <img
+                      src="/images/germanCustomerAward_2023.png"
+                      className="max-h-full w-auto object-contain"
+                      alt=""
+                    />
+                  </div>
+
+                  <div className="flex h-full items-center">
+                    <img
+                      src="/images/ntv_2023.png"
+                      className="max-h-full w-auto object-contain"
+                      alt=""
+                    />
+                  </div>
+
+                  <div className="flex h-full items-center">
+                    <img
+                      src="/images/besteOnlinePortale_2023.png"
+                      className="max-h-full w-auto object-contain"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Container>
+
+          <MouseIcon className="absolute bottom-5 left-1/2 size-7 -translate-x-1/2 rotate-180 animate-bounce text-white/50" />
+        </div>
       </div>
     </div>
   );
